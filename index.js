22a4a6ae50ecd4d76b0058bb008750d9ec9b1653
Sharifa Islam <sislam6@buffalo.edu>
2022-06-16 18:38:29 -0400

    initial upload

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam" ,
+    address: "11 Broadway",
+};
+function updateEmployeeWithKeyAndValue(Object, key, Value){
+    return{
+        ...Object,
+        [key]: Value,
+    };  
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const new_Object = Object.assign({}, employee);
+    delete new_Object[key];
+    return new_Object;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    Object.assign({}, employee);
+    delete employee[key];
+    return employee;
+}
